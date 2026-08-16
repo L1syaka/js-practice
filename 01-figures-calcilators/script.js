bc1fd973660b3d725f1124f5cb08eae0939f19e6
L1syaka <zorgichanel@gmail.com>
2026-08-16 16:34:19 +0300

    Add rectangle calculator

--- 01-figures-calcilators/script.js
+++ 01-figures-calcilators/script.js
@@ -9,26 +9,26 @@ function getPerimeter(num1, num2) {
 
 let input1 = document.querySelector('#input1');
 let input2 = document.querySelector('#input2');
-let inputs = document.querySelectorAll('#input1, #input2')
 let p = document.querySelector('p');
 let select = document.querySelector('select');
-
+let button = document.querySelector('button');
 
 select.addEventListener('change', () => {
     input1.value = '';
     input2.value = '';
+    p.textContent = ''
 });
 
 
-inputs.forEach(inp => {
-    inp.addEventListener('blur', function() {
-        if (input1.value === '' || input2.value === '') return;
-    
-        if (select.value === 'square') {
-            p.textContent = getSquare(+input1.value, +input2.value);
-        } else {
-            p.textContent = getPerimeter(+input1.value, +input2.value);
-        }
-    })
-});
+button.addEventListener('click', () => {
+    let a = input1.value;
+    let b = input2.value;
 
+    if (a === '' || b === '') return;
+
+    if (select.value === 'square') {
+        p.textContent = getSquare(+a, +b);
+    } else {
+        p.textContent = getPerimeter(+a, +b);
+    }
+});
