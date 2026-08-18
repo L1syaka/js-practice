0a097411f10b048ab42c1c9b948a41ab08ed88ac
L1syaka <zorgichanel@gmail.com>
2026-08-18 12:51:20 +0300

    Add guess number game

--- 03-guess-the-number/script.js
+++ 03-guess-the-number/script.js
@@ -1,1 +1,26 @@
 "use strict";
+
+function getRandom() {
+    let randomNum = Math.floor(Math.random() * (100 - 1 + 1)) + 1;
+    console.log(randomNum);
+    return randomNum;
+}
+
+let randomNum = getRandom();
+
+let input = document.querySelector('input');
+let p = document.querySelector('p');
+
+input.addEventListener('blur', function() {
+    let inp = +this.value;
+
+    if (this.value === '') return;
+
+    if(inp === randomNum) {
+        p.textContent = 'Correct!';
+    } else if (inp < randomNum) {
+        p.textContent = 'Enter a larger number'
+    } else {
+        p.textContent = 'Enter a smaller number'
+    }
+});
