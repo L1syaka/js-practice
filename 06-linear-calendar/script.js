811c291f61e063ae99ba05410b3f1207c20ad2c3
L1syaka <zorgichanel@gmail.com>
2026-08-20 16:06:22 +0300

    Add linear calendar

--- 06-linear-calendar/script.js
+++ 06-linear-calendar/script.js
@@ -1,2 +1,26 @@
 "use strict";
 
+function showLinearCalendar() {
+    let now = new Date();
+    let day = 1;
+    
+    let ul = document.createElement('ul');
+    document.body.append(ul);
+
+    while(true) {
+        let monthDays = new Date(now.getFullYear(), now.getMonth(), day);
+
+        if(!(now.getMonth() === monthDays.getMonth())) {
+            return;
+        }
+
+        let li = document.createElement('li');
+        li.textContent = monthDays.getDate();
+
+        
+        ul.append(li);
+        day++;
+    }
+
+}
+showLinearCalendar();
