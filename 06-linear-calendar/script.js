b6a2307181d7dea767dc8cfe360759f52a25274c
L1syaka <zorgichanel@gmail.com>
2026-08-20 16:33:12 +0300

    Add current month and year heading

--- 06-linear-calendar/script.js
+++ 06-linear-calendar/script.js
@@ -2,27 +2,47 @@
 
 function showLinearCalendar() {
     let now = new Date();
-    let day = 1;
+    let monthsArr = [
+        "January",
+        "February",
+        "March",
+        "April",
+        "May",
+        "June",
+        "July",
+        "August",
+        "September",
+        "October",
+        "November",
+        "December"
+    ]
     
+    let calendarContainer = document.createElement('div');
+    let calendarCurrentDate = document.createElement('div')
     let ul = document.createElement('ul');
-    document.body.append(ul);
-
-    while(true) {
-        let monthDays = new Date(now.getFullYear(), now.getMonth(), day);
-
-        if(!(now.getMonth() === monthDays.getMonth())) {
+    
+    calendarContainer.classList.add('calendar-container')
+    calendarCurrentDate.textContent = `${monthsArr[now.getMonth()]} | ${now.getFullYear()}`;
+    
+    document.body.append(calendarContainer);
+    calendarContainer.append(calendarCurrentDate);
+    calendarContainer.append(ul);
+    
+    for(let day = 1; ;day++) {
+        let currentMonth = new Date(now.getFullYear(), now.getMonth(), day);
+        
+        if(!(now.getMonth() === currentMonth.getMonth())) {
             return;
         }
 
         let li = document.createElement('li');
-        li.textContent = monthDays.getDate();   
+        li.textContent = currentMonth.getDate();
 
-        if(monthDays.getDate() === now.getDate()) {
+        if(currentMonth.getDate() === now.getDate()) {
             li.classList.add('currentDay');
         }
 
         ul.append(li);
-        day++;
     }
 
 }
