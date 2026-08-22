fe18475115ab4ffba5601eb7e670641b9404ca21
L1syaka <zorgichanel@gmail.com>
2026-08-22 20:51:35 +0300

    Add spoiler

--- 12-spoiler/script.js
+++ 12-spoiler/script.js
@@ -1,2 +1,23 @@
 "use strict";
 
+let showSpoiler = document.querySelector('.toggle');
+
+function revealSpoiler(thisTarget) {
+    let target = thisTarget.parentElement.nextElementSibling;
+
+    if (!target.classList.contains('spoiler')) return;
+
+    if (!target.classList.contains('active')) {
+        target.classList.add('active');
+        thisTarget.textContent = 'Hide spoiler';
+    } else {
+        target.classList.remove('active');
+        thisTarget.textContent = 'Show spoiler'
+    }
+}
+
+showSpoiler.addEventListener('click', function(event) {
+    event.preventDefault();
+
+    revealSpoiler(this)
+});
