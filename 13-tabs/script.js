2a3967c379e3b674bed410a07c319ba3e2e2b5ff
L1syaka <zorgichanel@gmail.com>
2026-08-22 22:25:10 +0300

    Add tabs

--- 13-tabs/script.js
+++ 13-tabs/script.js
@@ -1,1 +1,32 @@
 "use strict";
+
+let buttons = document.querySelectorAll('#parent .menu a');
+let tabs = document.querySelectorAll('#parent .tabs .tab');
+let menu = document.querySelector('#parent .menu');
+
+function switchTabs(linkTarget) {
+
+    buttons.forEach((link, index) => {
+        if (link.classList.contains('active')) {
+
+            if(linkTarget === link) return;
+
+            link.classList.remove('active');
+            tabs[index].classList.remove('active');
+        }
+    });
+
+    let tabIndex = [...buttons].findIndex((link) => linkTarget === link);
+
+    linkTarget.classList.add('active');
+    tabs[tabIndex].classList.add('active');
+}
+
+menu.addEventListener('click', function(event) {
+    event.preventDefault();
+    let a = event.target.closest('a');
+    
+    if(!a) return;
+
+    switchTabs(a)
+});
