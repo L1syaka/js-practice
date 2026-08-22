ecd44c50f01659e4a9d1a4cc029f5abd4121fef8
L1syaka <zorgichanel@gmail.com>
2026-08-22 23:35:54 +0300

    Add accordion tabs

--- 14-accordion-tabs/script.js
+++ 14-accordion-tabs/script.js
@@ -1,1 +1,29 @@
 "use strict";
+
+let parent = document.getElementById('parent')
+let tabs = parent.getElementsByClassName('tab')
+let links = parent.getElementsByClassName('link')
+
+let test = document.querySelector('a');
+
+function showAccordionTab(target) {
+    let tabTarget = target.parentElement;
+
+    if(tabTarget.classList.contains('active')) {
+        tabTarget.classList.remove('active');  
+    } else {
+        for(let tab of tabs) {
+            tab.classList.remove('active');
+        }
+        tabTarget.classList.add('active');
+    }
+
+}
+
+parent.addEventListener('click', function(event) {
+    let target = event.target.closest('.link');
+
+    if(!target) return;
+
+    showAccordionTab(target);
+});
