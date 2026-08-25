e79d064a0f636820e1b8a6c2f9f09c507ea5452a
L1syaka <zorgichanel@gmail.com>
2026-08-25 05:40:27 +0300

    add game modes

--- 15-cities-word-chain-game/script.js
+++ 15-cities-word-chain-game/script.js
@@ -2,40 +2,96 @@
 
 let field = document.querySelector('#field');
 let message = document.querySelector('#message');
+let mode = document.querySelector('#mode-select input:checked');
 
 let cities = [];
+let allCities = [
+  "Kyiv",
+  "Kharkiv",
+  "Odesa",
+  "Dnipro",
+  "Donetsk",
+  "Zaporizhzhia",
+  "Lviv",
+  "Kryvyi Rih",
+  "Mykolaiv",
+  "Sevastopol",
+  "Mariupol",
+  "Luhansk",
+  "Vinnytsia",
+  "Makiivka",
+  "Simferopol",
+  "Kherson",
+  "Poltava",
+  "Chernihiv",
+  "Cherkasy",
+  "Zhytomyr",
+];
 
-function gameTurns(inputTarget) {
-    let input = inputTarget.value;
-
+function citiesCheck(target) {
     if (cities.length > 0) {
         let last = cities[cities.length - 1];
         let lastLetter = last[last.length - 1];
 
-        if (cities.includes(input)) {
+        if (cities.includes(target)) {
             message.textContent = 'This city already used!'
-            return;
+            return false;
         }
 
-        if (input[0].toLowerCase() === lastLetter.toLowerCase()) {
-            message.textContent = `Last word: ${input}`
-            cities.push(input)
+        if (target[0].toLowerCase() === lastLetter.toLowerCase()) {
+            message.textContent = `Last word: ${target}`
+            cities.push(target)
+            return true;
 
         } else {
             message.textContent = `Incorrect city`;
+            return false;
         }
     } else {
-        message.textContent = `First word: ${input}`
-        cities.push(input)
-        console.log(cities)
+        message.textContent = `First word: ${target}`;
+        cities.push(target);
+        return true;
+    }
+}
+
+function playerTurn(inputTarget) {
+    let input = inputTarget.value[0].toUpperCase() + inputTarget.value.slice(1);
+    
+    if (mode.value === 'computer') {
+        if (!allCities.includes(input)) {
+            message.textContent = 'Computer doesnt have this city';
+            return;
+        }
+        
+        if (citiesCheck(input)) {
+            computerTurn();
+            console.log(cities)
+        } else {
+            return;
+        }
+    } else {
+        citiesCheck(input)
     }
 
-    inputTarget.value = ''
+    inputTarget.value = '';
+}
 
+function computerTurn() {
+    let last = cities[cities.length - 1];
+    let lastLetter = last[last.length - 1];
+
+    for (let city of allCities) {
+        if (city[0].toLowerCase() === lastLetter && !cities.includes(city)) {
+            citiesCheck(city);
+            return;
+        }
+    }
+
+    message.textContent = 'No more available cities'
 }
 
 field.addEventListener('keypress', function(event) {
     if(event.key !== 'Enter') return;
-
-    gameTurns(this)
+    if(field.value === '') return;
+    playerTurn(this)
 });
