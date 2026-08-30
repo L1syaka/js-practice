00e6c88680707578a8121b7e70b28fdd8e141949
L1syaka <zorgichanel@gmail.com>
2026-08-30 06:54:06 +0300

    generate questions from array

--- 18-tests/script.js
+++ 18-tests/script.js
@@ -6,9 +6,13 @@ let answers = [
     'answer 3'
 ];
 
+let questions = [
+    'question 1',
+    'question 2',
+    'question 3'
+];
 
 let test = document.querySelector('#test');
-let inputs = test.querySelectorAll('input');
 let result = document.querySelector('#resBtn');
 
 function checkAnswer(input, index) {
@@ -21,6 +25,22 @@ function checkAnswer(input, index) {
     }
 }
 
+;(function createQuestions() {
+    for (let question of questions) {
+        let div = document.createElement('div');
+
+        let p = document.createElement('p');
+        p.textContent = question; 
+        div.append(p);
+
+        let input = document.createElement('input');
+        div.append(input);
+
+        test.append(div);
+    }
+})();
+
 result.addEventListener('click', function() {
+    let inputs = test.querySelectorAll('input');
     inputs.forEach((input, index) => checkAnswer(input, index))
 });
