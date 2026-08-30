28cdf0f347f872d173abf459262aa29b6f1e09cf
L1syaka <zorgichanel@gmail.com>
2026-08-30 05:52:00 +0300

    add tests

--- 18-tests/script.js
+++ 18-tests/script.js
@@ -1,2 +1,24 @@
 "use strict";
 
+let test = document.querySelector('#test');
+let inputs = test.querySelectorAll('input');
+
+function checkAnswer(input) {
+    if (input.value === input.dataset.answer) {
+        input.classList.add('right');
+        input.disabled = true;
+    } else {
+        input.classList.add('wrong');
+        input.disabled = true;
+    }
+
+    
+}
+
+inputs.forEach(input => {
+    input.addEventListener('keypress', function(event) {
+        if(event.key !== "Enter") return;
+
+        checkAnswer(input);
+    });
+});
