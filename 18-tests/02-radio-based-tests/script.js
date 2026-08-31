b28b13ae58d7bf6730aaee87be0ccafba6530567
L1syaka <zorgichanel@gmail.com>
2026-08-31 07:13:35 +0300

    add answers array

--- 18-tests/02-radio-based-tests/script.js
+++ 18-tests/02-radio-based-tests/script.js
@@ -1,20 +1,30 @@
 "use strict";
 
+let answers = [0, 1, 2];
+
 let test = document.querySelector('#test');
-let radios = test.querySelectorAll('input');
+let questions = test.querySelectorAll('div')
 let button = document.querySelector('#checkRes');
 
-function checkAnswer(radio) {
-    if (radio.hasAttribute('data-right')) {
-        radio.classList.add('right')
-    } else {
-        radio.classList.add('wrong')
-    }
+function checkAnswer(radio, questionIndex, radioIndex) {
+        if (answers[questionIndex] === radioIndex) {
+            radio.classList.add('right')
+        } else {
+            radio.classList.add('wrong')
+        }
 }
 
 
 button.addEventListener('click', function() {
-    for (let radio of radios) {
-            checkAnswer(radio);
-    }
+    questions.forEach((question, questionIndex) => {
+
+       let radios = question.querySelectorAll('input');
+
+        radios.forEach((radio, radioIndex) => {
+            
+            if(radio.checked) {
+                checkAnswer(radio, questionIndex, radioIndex)
+            }
+        });
+    });
 });
