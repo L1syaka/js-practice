02c2eaff194dc5657e304a2e01972f5c6d014a62
L1syaka <zorgichanel@gmail.com>
2026-08-31 06:40:46 +0300

    add radio test

--- 18-tests/02-radio-based-tests/script.js
+++ 18-tests/02-radio-based-tests/script.js
@@ -1,1 +1,19 @@
 "use strict";
+
+let test = document.querySelector('#test');
+let radios = test.querySelectorAll('input');
+
+function checkAnswer(radio) {
+    console.log(radio.g);
+    if (radio.hasAttribute('data-right')) {
+        radio.classList.add('right')
+    } else {
+        radio.classList.add('wrong')
+    }
+}
+
+for (let radio of radios) {
+    radio.addEventListener('click', function() {
+        checkAnswer(radio);
+    });
+}
