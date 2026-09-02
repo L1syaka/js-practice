ce7e07b07cacedb55f2798b2390216e64e8bcfb6
L1syaka <zorgichanel@gmail.com>
2026-09-02 14:49:02 +0300

    add text slider

--- 19-sliders/01-text-slider/script.js
+++ 19-sliders/01-text-slider/script.js
@@ -1,1 +1,17 @@
 "use strict";
+
+let texts = ['text1', 'text2', 'text3'];
+
+function textSlider() {
+    let slider = document.querySelector('#slider');
+    let index = 0;
+
+    
+    let timer = setInterval(() => {
+        slider.textContent = texts[index];
+        index++;
+        
+        if(index >= texts.length) index = 0;
+    }, (1000));
+}
+textSlider()
