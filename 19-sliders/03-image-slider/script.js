4e76fb83d7204711dc401977715b8ff59fdb5da8
L1syaka <zorgichanel@gmail.com>
2026-09-03 10:26:17 +0300

    add image slider

--- 19-sliders/03-image-slider/script.js
+++ 19-sliders/03-image-slider/script.js
@@ -1,1 +1,16 @@
 "use strict";
+
+let slider = document.querySelector('#slider');
+
+let images = ['1.jpg', '2.jpg', '3.jpg'];
+
+function imgSliderTimer() {
+    let index = 0;
+    slider.src = `images/${images[index++]}`;
+
+    setInterval(() => {
+        slider.src = `images/${images[index++]}`;
+        if (index >= images.length) index = 0;
+    }, 1000);
+}
+imgSliderTimer();
