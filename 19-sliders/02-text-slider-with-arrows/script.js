da0ccfb69dc3c1925d6550f64c14a1419132c779
L1syaka <zorgichanel@gmail.com>
2026-09-03 08:28:54 +0300

    add text slider with arrows

--- 19-sliders/02-text-slider-with-arrows/script.js
+++ 19-sliders/02-text-slider-with-arrows/script.js
@@ -1,2 +1,35 @@
 "use strict";
 
+let texts = ['text1', 'text2', 'text3'];
+
+let left = document.querySelector('#left');
+let right = document.querySelector('#right');
+let slider = document.querySelector('#slider');
+
+let i = 0;
+
+function changeTextOnSlider() {
+    console.log(i);
+    slider.textContent = texts[i];    
+}
+changeTextOnSlider();
+
+
+
+left.addEventListener('click', function(event) {
+    event.preventDefault();
+
+    if(i > 0) {
+        i--;
+        changeTextOnSlider();
+    }
+});
+
+right.addEventListener('click', function(event) {
+    event.preventDefault();
+    
+    if(i < texts.length - 1) {
+        i++;
+        changeTextOnSlider();
+    }
+});
